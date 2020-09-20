50e5a588931e27dbffd97b8093db8ff194d3bdea
Steven Gregory <sgregory@gmail.com>
2020-09-20 15:20:01 -0500

    feat: added create file message

--- vscode-extension-export.sh
+++ vscode-extension-export.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+green='\033[0;32m'
+no_color='\033[0m'
+
 for extension in $(code --list-extensions)
 do
   echo code --install-extension $extension >> vscode-extension-install.sh
 done
+
+echo -e ${green}CREATE${no_color} vscode-extension-install.sh
 
 exit 0
