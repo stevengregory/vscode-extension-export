3f748cffa1d629cfa956f0ec2013b57c5b545ddb
Steven Gregory <sgregory@gmail.com>
2020-11-29 13:38:14 -0600

    fix: only create install script if not present

--- scripts/vscode-extension-export.sh
+++ scripts/vscode-extension-export.sh
@@ -4,12 +4,11 @@ source ./scripts/message.sh
 
 file=vscode-extension-install.sh
 
-for extension in $(code --list-extensions)
-do
-  echo code --install-extension $extension >> $file
-done
-
-if [ -f $file ]; then
+if [ ! -f $file ]; then
+  for extension in $(code --list-extensions)
+  do
+    echo code --install-extension $extension >> $file
+  done
   get_message $file
 fi
 
