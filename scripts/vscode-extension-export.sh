b45637ddf99ed483b88509bf37ecd059545ed2f7
Steven Gregory <sgregory@gmail.com>
2020-12-06 19:10:52 -0600

    refactor: extract logic to functions & clean-up

--- scripts/vscode-extension-export.sh
+++ scripts/vscode-extension-export.sh
@@ -2,19 +2,25 @@
 
 source ./scripts/message.sh
 
-file=dist/vscode-extension-install.sh
+clear_build() {
+  if [ -d dist ]; then
+    rm -rf dist
+  fi
+}
 
-if [ -d dist ]; then
-  rm -rf dist;
-fi
+export_extensions() {
+  file=dist/vscode-extension-install.sh
+  if [ ! -d dist ]; then
+    mkdir -p dist
+    for extension in $(code --list-extensions)
+    do
+      echo code --install-extension $extension >> $file
+    done
+    get_message $file
+  fi
+}
 
-if [ ! -d dist ]; then
-  mkdir -p dist;
-  for extension in $(code --list-extensions)
-  do
-    echo code --install-extension $extension >> $file
-  done
-  get_message $file
-fi
+clear_build
+export_extensions
 
 exit 0
