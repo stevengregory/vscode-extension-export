8e64d9e1ed6a63ca53c253f80f2644a193b130df
Steven Gregory <sgregory@gmail.com>
2020-12-06 19:18:27 -0600

    refactor: abstract logic for getting extensions

--- scripts/vscode-extension-export.sh
+++ scripts/vscode-extension-export.sh
@@ -12,12 +12,16 @@ export_extensions() {
   file=dist/vscode-extension-install.sh
   if [ ! -d dist ]; then
     mkdir -p dist
-    for extension in $(code --list-extensions)
+    for extension in $(get_extensions)
     do
       echo code --install-extension $extension >> $file
     done
     get_message $file
   fi
+}
+
+get_extensions() {
+  code --list-extensions
 }
 
 clear_build
